162f5f6a019d650fd03f238bc5071fe6b99d427b
Javier Flores <jffc.dev@gmail.com>
2024-01-06 20:57:24 -0500

    feat: checkCollision function added

---
+++ 01_tetrisTest/main.js
@@ -0,0 +1,121 @@
+import './style.css'
+
+
+//initialize canvas
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGT = 30
+
+canvas.width = BOARD_WIDTH * BLOCK_SIZE
+canvas.height = BOARD_HEIGT * BLOCK_SIZE
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// board
+
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+]
+
+
+//current piece
+
+const piece = {
+  position: {x: 5, y: 5},
+  shape: [
+    [1, 1],
+    [1, 1]
+  ]
+}
+
+
+// game loop
+const update = () => {
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+const draw = () => {
+  context.fillStyle = "#000"
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1){
+        context.fillStyle = '#fff'
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1){
+        context.fillStyle = 'yellow'
+        context.fillRect(piece.position.x + x, piece.position.y + y, 1, 1)
+      }
+    })
+  })
+}
+
+document.addEventListener('keydown', event => {
+  if(event.key === 'ArrowLeft'){
+    piece.position.x--
+    if(checkCollision())
+      piece.position.x++
+  }
+  if(event.key === 'ArrowRight'){
+    piece.position.x++
+    if(checkCollision())
+      piece.position.x--
+  }
+  if(event.key === 'ArrowDown'){
+    piece.position.y++
+    if(checkCollision())
+      piece.position.y--
+  }
+
+  console.log(piece.position);
+})
+
+const checkCollision = () => {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+    })
+  })
+}
+update()
+
