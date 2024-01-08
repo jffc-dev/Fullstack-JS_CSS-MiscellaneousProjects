0727a363bc21458e35b878a40cd32c749ccf3610
Javier Flores <jffc.dev@gmail.com>
2024-01-07 19:13:50 -0500

    feat: rotate pieces, solidifyPiece and removeRows added

--- 01_tetrisTest/main.js
+++ 01_tetrisTest/main.js
@@ -60,9 +60,54 @@ const piece = {
   ]
 }
 
+const PIECES = [
+  [
+    [1, 1, 1]
+  ],
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [
+    [1, 0, 0],
+    [1, 1, 1]
+  ],
+]
+
 
 // game loop
-const update = () => {
+// const update = () => {
+//   draw()
+//   window.requestAnimationFrame(update)
+// }
+
+let dropCounter = 0
+let lastTime = 0
+
+const update = (time = 0) => {
+  const deltaTime = time - lastTime
+  lastTime = time
+
+  dropCounter += deltaTime
+
+  if(dropCounter > 1000){
+    piece.position.y++
+    dropCounter = 0
+
+    if(checkCollision()){
+      piece.position.y--
+      solidifyPiece()
+      removeRows()
+    }
+  }
   draw()
   window.requestAnimationFrame(update)
 }
@@ -103,11 +148,32 @@ document.addEventListener('keydown', event => {
   }
   if(event.key === 'ArrowDown'){
     piece.position.y++
-    if(checkCollision())
+    if(checkCollision()){
       piece.position.y--
+      solidifyPiece()
+      removeRows()
+    }
   }
+  if(event.key === 'ArrowUp'){
+    const rotated = []
 
-  console.log(piece.position);
+    for(let i = 0; i < piece.shape[0].length; i++){
+      const row = []
+
+      for(let j = piece.shape.length - 1; j >= 0; j--){
+        row.push(piece.shape[j][i])
+      }
+
+      rotated.push(row)
+    }
+
+    const previousShape = piece.shape
+
+    piece.shape = rotated
+
+    if(checkCollision())
+    piece.shape = previousShape
+  }
 })
 
 const checkCollision = () => {
@@ -117,5 +183,41 @@ const checkCollision = () => {
     })
   })
 }
-update()
 
+const solidifyPiece = () => {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1)
+        board[y + piece.position.y][x + piece.position.x] = 1
+    })
+  })
+
+  //get random shape
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+  //reset position
+  piece.position.y = 0
+  piece.position.x = 0
+
+  if(checkCollision()){
+    window.alert('Game over')
+    board.forEach((row) => row.fill(0))
+  }
+}
+
+const removeRows = () => {
+  const rowsToRemove = []
+
+  board.forEach((row, y) => {
+    if(row.every(value => value === 1)){
+      rowsToRemove.push(y)
+    }
+  })
+
+  rowsToRemove.forEach(y => {
+    board.splice(y,1)
+    const newRow = Array(BOARD_WIDTH).fill(0)
+    board.unshift(newRow)
+  })
+}
+
+update()
