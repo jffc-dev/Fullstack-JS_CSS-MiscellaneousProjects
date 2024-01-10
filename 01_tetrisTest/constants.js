32631447ec6f44cb7d98194684fef54e35599ff0
Javier Flores <jffc.dev@gmail.com>
2024-01-09 23:35:48 -0500

    feat: constants file added

---
+++ 01_tetrisTest/constants.js
@@ -0,0 +1,23 @@
+export const PIECES = [
+  [
+    [1, 1, 1]
+  ],
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [
+    [1, 0, 0],
+    [1, 1, 1]
+  ]
+]
+
+export const COLORS = ['yellow', 'red', 'deeppink', 'green', 'blue', 'orange', 'white', 'greenyellow', 'cyan', 'salmon', 'teal', 'gray', 'magenta', 'blueviolet', 'brown', 'khaki', 'chocolate', 'burlywood']
